b2f73c79346cb487e7bfcde0a8d93521d2f0897c
Ronald R. Gemao <ronadmad7@gmail.com>
2014-01-27 16:46:42 +0800

    Created test for program controller (1904)

--- test/public/spec/controllers/ProgramsControllerTest.js
+++ test/public/spec/controllers/ProgramsControllerTest.js
@@ -6,46 +6,41 @@ define( function ( require ) {
 	var Backbone   = require( 'backbone' );
 	var Marionette = require( 'marionette' );
 	var async      = require( 'async' );
+	var _ 		   = require( 'underscore');
 
 	var Controller   = require( 'controllers/ProgramsController' );
-	var Communicator = require( 'Communicator' );
+	var Collection = require( 'collections/ProgramCollection' );
+	var template   = require( 'text!templates/Grid/GridLayoutView.html' );
 
-	describe( 'ProgramsController Test', function () {
-		var controller;
-		var contentRegion = new Marionette.Region( { 'el' : $( '#center-region' ) } );
+	describe( 'Programs Controller', function () {	
+		var controller = new Controller();
+		var collection = new Collection();
 
-		// router spy for this.App.Router.navigate() assertions
-		var navigateURL = '';
-		var router = {
-			'navigate' : function ( url ) {
-				navigateURL = url;
-			}
-		};
-
-		var createController = function () {
-			controller = new Controller( {
-				'App'     : {
-					'Router' : router
-				},
-				'regions' : {
-					'content' : contentRegion
-				},
-				'Communicator' : Communicator
+		it( 'should be an instance', function () {
+			controller.should.be.an.instanceof( Controller );
+		} );
+		it( 'should have functions', function () {
+			controller.show.should.be.a.function;
+			controller.initialize.should.be.a.function;
+			controller.fetchCollection.should.be.a.function;
+		} );
+		it( 'show region', function ( done ) {
+			var region = new Marionette.Layout.extend( {
+				'template'  : _.template( template ), 
+				regions:{ mainRegion : '#grid-content' }	
 			} );
-		};
-		var destroyController = function () {
-			contentRegion.reset();
-			controller.stopListening();
-			controller = undefined;
-			Backbone.history.navigate( '' );
-			navigateURL = '';
-		};
 
-		describe( 'show()', function () {
-			before( createController );
-			after( destroyController );
+			var showRegion = controller.show( region , Collection );
+			showRegion.should.be.an.instance;
 
+			collection.fetch( { 
+				success: function( col, res, opt ) {
+					col.length.should.be.above(0);
+					done();
+				}
+			} );
 		} );
 	} );
 
 } );
+ 
