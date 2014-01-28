740d0c37908d290360805a3500d76e3e48740f55
Ronald R. Gemao <ronadmad7@gmail.com>
2014-01-28 16:32:53 +0800

    Revised test for program controller (1904)

--- test/public/spec/controllers/ProgramsControllerTest.js
+++ test/public/spec/controllers/ProgramsControllerTest.js
@@ -1,27 +1,41 @@
-define( function ( require ) {
-	'use strict';
+define(function(require) {
+    'use strict';
+ 
+    var $ = require( 'jquery' );
+    var should = require( 'chai' ).should();
+    var Backbone = require( 'backbone' );
+    var Marionette = require( 'marionette' );
+    var async = require( 'async' );
 
-	var $          = require( 'jquery' );
-	var should     = require( 'chai' ).should();
-	var Backbone   = require( 'backbone' );
-	var Marionette = require( 'marionette' );
-	var async      = require( 'async' );
+    var Controller = require( 'controllers/ProgramsController' );
+    var Collection = require( 'collections/ProgramCollection' );
 
-	var Controller = require( 'controllers/ProgramsController' );
-
-	describe( 'Programs Controller', function () {
-		var controller = new Controller();
-		it( 'should be an instance', function () {
-			controller.should.be.an.instanceof( Controller );
-		} );
-		it( 'should have functions', function () {
-			controller.show.should.be.a.function;
-			controller.initialize.should.be.a.function;
-		} );
-		it( 'show region', function () {
-			var region = new Marionette.Region( { el : $( '#center-region') });
-			var showRegion = controller.show( region );
-			showRegion.should.be.an.instance;
-		} );
-	} );
-} );
+    describe('Programs Controller', function() {
+        var controller = new Controller();
+        var collection = new Collection();
+        it('should be an instance', function() {
+            controller.should.be.an.instanceof(Controller);
+        });
+        it('should have functions', function() {
+            controller.show.should.be.a.function;
+            controller.initialize.should.be.a.function;
+            controller.fetchCollection.should.be.a.function;
+            controller.showSegments.should.be.a.function;
+        });
+        it('show region', function() {
+            var region = new Marionette.Region({
+                el: $('#center-region')
+            });
+            var showRegion = controller.show( region, Collection );
+            showRegion.should.be.an.instance;
+        });
+        it('should fetch json data', function( done ) {
+            collection.fetch({
+                success: function( col, res, opt ) {
+                    col.length.should.be.above(0);
+                    done();
+                }
+            });
+        });
+    });
+});
